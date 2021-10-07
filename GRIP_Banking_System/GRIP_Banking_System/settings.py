23e8d45c0afbdd2834843b6737bea62987cecb4a
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-10-07 12:00:32 +0530

    Update settings.py

--- GRIP_Banking_System/GRIP_Banking_System/settings.py
+++ GRIP_Banking_System/GRIP_Banking_System/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'GRIP_Banking_System.urls'
