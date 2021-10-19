27437fe668e20a4f582f513c8fe2b967cbcdcfc5
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-10-19 14:58:26 +0530

    Update settings.py

--- GRIP_Banking_System/settings.py
+++ GRIP_Banking_System/settings.py
@@ -119,11 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'GRIP_Banking_System/static')
+STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'GRIP_Banking_System/static')
 # ]
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
