9119e9d46f9234e3629068e030efb5ee205e4edc
satvikakoti <55699106+satvikakoti@users.noreply.github.com>
2021-10-19 14:50:06 +0530

    Update settings.py

--- GRIP_Banking_System/settings.py
+++ GRIP_Banking_System/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'GRIP_Banking_System/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'GRIP_Banking_System/static')
+# ]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
